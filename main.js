aec202b02d376d457ced1787c7f0515668a713fb
nawzaysfinah <nawzaysfinah@gmail.com>
2023-04-11 13:11:07 +0800

    on/off coloring functionality

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-let color = "black"
+let color = "black";
+let click = true;
 
 function populateBoard(size) {
     let board = document.querySelector('.board');
@@ -28,11 +29,13 @@ function changeSize(input) {
 }
 
 function colorSquare() {
-    if(color === 'random') {
-        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
-    }
-    else {
-        this.style.backgroundColor = color;
+    if (click) {
+        if(color === 'random') {
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+        }
+        else {
+            this.style.backgroundColor = color;
+        }
     }
 }
 function changeColor(choice) {
@@ -44,3 +47,14 @@ function resetBoard() {
     let squares = board.querySelectorAll('div');
     squares.forEach((div) => div.style.backgroundColor = "white");
 }
+
+document.querySelector("body").addEventListener("click", (e) => {
+    if (e.target.tagName != 'button') {
+        click = !click;
+        if(click) {
+            document.querySelector('.mode').textContent = "Mode: Coloring"
+        } else {
+            document.querySelector('.mode').textContent = "Mode: Not Coloring"
+        }
+    }
+});
